7d1c5b340520874b03e84ea9f59777ba780f963f
Nikita Balabaev <xakep139@users.noreply.github.com>
2018-06-06 16:37:44 +0700

    [AMS-1836] Fix memory allocation of binaries collector (#170)
    * [AMS-1836] change runtimes to ubuntu.14.04-x64

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,4 @@ set -e
 dotnet restore
 dotnet test tests/VStore.UnitTests -c release
 rm -rf $(pwd)/publish/vstore
-dotnet publish src/VStore.Host/project.json -c release -r debian.9-x64 -o $(pwd)/publish/vstore
+dotnet publish src/VStore.Host/project.json -c release -r ubuntu.14.04-x64 -o $(pwd)/publish/vstore
